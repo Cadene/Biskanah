d72438fcf7fb4c1a396fc85f69cf0138d0c348ae
Tamazy <tamazy@laposte.net>
2014-03-27 00:01:28 +0100

    Cancel app/* gitignore

---
+++ www/app/Console/models/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'root',
+		'password' => 'enaifos',
+		'database' => 'biskanahv1',
+	);
+}
+?>
