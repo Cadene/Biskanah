fb7d408f91f01767ebdaad2d3cfeec200bed9685
Tamazy <tamazy@laposte.net>
2014-07-23 13:33:53 +0200

    Console with CakePhp 1

--- Console/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    date_default_timezone_set('UTC');
-
-//define( 'DATABASE_host' , 'mysql:host/Applications/MAMP/tmp/mysql/mysql.sock' ); // le chemin vers le serveur
-define( 'DATABASE_host' ,'mysql:host=localhost');
-define( 'DATABASE_port' , '8889' );
-define( 'DATABASE_nom_bd' , 'biskanah' ); // le nom de votre base de données
-define( 'DATABASE_utilisateur' , 'root' ); // nom d'utilisateur pour se connecter
-define( 'DATABASE_mot_passe' , 'root' ); // mot de passe de l'utilisateur pour se connecter
---
+++ www/app/Console/Command/DropShell.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Tamazy
+ * Date: 23/07/2014
+ * Time: 12:51
+ * To change this template use File | Settings | File Templates.
+ */
